72190eae86137ad979f2a4de4017d4bce7db2a1d
AurickMikleton <228803305+AurickMikleton@users.noreply.github.com>
2026-01-27 19:48:42 -0700

    finished data scafold

--- prototype/main.py
+++ prototype/main.py
@@ -1,6 +1,7 @@
 from rdflib import Graph, URIRef
 from itertools import groupby
 from operator import itemgetter
+from datetime import datetime
 
 data_directory = "../data/"
 
@@ -14,12 +15,32 @@ HAS_AVAILABILITY = URIRef(EX + "hasAvailability")
 AVAILABLE_FROM = URIRef(EX + "availableFrom")
 AVAILABLE_UNTIL = URIRef(EX + "availableUntil")
 
+@dataclass
+class Room:
+    uri: URIRef
+    capacity: int
+    free: List[Tuple[datetime, datetime]] # (start time, end time) pairs for availability
+
+@dataclass
+class Course:
+    uri: URIRef
+    exam_minutes: int
+    enrollment: int
+    min_room_capacity: int
+
 def load_graph(path: Path) -> Graph:
     graph = Graph()
     graph.parse(str(path), format="turtle")
     return graph
 
-def main():
+def get_enrollment_counts(students: Graph) -> Dict[URIRef, int]:
+    counts: Dict[URIRef, int] = {}
+    for _, _, cls in g_students.triples((None, ENROLLED_IN, None)):
+        if isinstance(cls, URIRef):
+            counts[cls] = counts.get(cls, 0) + 1
+    return counts
+
+def main() -> None:
     students_path = data_directory + "students.ttl"
     classes_path = data_directory + "classes.ttl"
     rooms_path = data_directory + "rooms.ttl"
@@ -29,7 +50,8 @@ def main():
     rooms = load_graph(rooms_path)
 
     # Enrollment counts -> Dict[URIRef, int]
-    # Rooms list
-    # Courses list
+    enrollment_counts = get_enrollment_counts(students)
+    # Rooms list -> List[Room]
+    # Courses list -> List[Course]
 
     # Greedy algorithm
